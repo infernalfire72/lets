81b44e19fe3e67ef2f9f3a960ea7e5483acf90f3
Josh Smith <cmyuisuki@gmail.com>
2019-02-18 06:58:59 -0500

    b

--- objects/rxscore.pyx
+++ objects/rxscore.pyx
@@ -206,7 +206,7 @@ class score:
 			self.rank,
 			self.date)
 
-	def setCompletedStatus(self):
+	def setCompletedStatus(self, b = None):
 		"""
 		Set this score completed status and rankedScoreIncrease
 		"""
@@ -214,6 +214,10 @@ class score:
 		if self.passed == True and scoreUtils.isRankable(self.mods):
 			# Get userID
 			userID = userUtils.getID(self.playerName)
+
+		# Create beatmap object
+		if b is None:
+			b = beatmap.beatmap(self.fileMd5, 0)
 
 			# Make sure we don't have another score identical to this one
 			duplicate = glob.db.fetch("SELECT id FROM scores_relax WHERE userid = %s AND beatmap_md5 = %s AND play_mode = %s AND time = %s AND score = %s LIMIT 1", [userID, self.fileMd5, self.gameMode, self.date, self.score])
